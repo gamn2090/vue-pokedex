600c6d5e34692072e1259a6612aecc25f80d27ad
Gustavo Mattey <gamn2090@gmail.com>
2022-04-08 23:58:54 -0500

    initial commit

---
+++ src/views/PokemonView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="about">
+    <div v-if="pokemon"
+      class="w-3/12 m-auto bg-gray-300 mt-4 shadow-2xl flex justify-center flex-col items-center">
+      <h3 class="text-2xl text-green-800 uppercase">{{pokemon.name}}</h3>
+      <div class="flex justify-center">
+        <img class="w-48" :src="pokemon.sprites.front_shiny" alt="front">
+        <img class="w-48" :src="pokemon.sprites.back_shiny" alt="back">        
+      </div>
+      <h3 class="text-red-400">Types</h3>
+      <div v-for="(type,idx) in pokemon.types" :key="idx">
+        <h5 class="text-blue-900">{{type.type.name}}</h5>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { useRoute } from 'vue-router'
+import {reactive, toRefs} from 'vue'
+
+export default {
+  setup() {
+    const route = useRoute()
+    
+    const state = reactive({
+      pokemon: null
+    })
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${route.params.slug}/`)
+      .then((res)=> res.json())
+      .then((data)=> {
+        //console.log(data)
+        state.pokemon = data;
+      })
+
+      return { ...toRefs(state)}
+
+  },
+}
+</script>
